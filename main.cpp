33f9a333413ee4aa810b7dbe29f766c33ee45901
Bodhi Cummings <112513201+BodhiCummings@users.noreply.github.com>
2023-02-07 04:34:34 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// Monday-Week-1
+// Due: Friday, February 10, 11:59 PM
+/*************************************************
+Student First Name: Bodhikhan
+Student Last Name: Cummings
+**************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!\n";
+}
+
+/*********************************************
+// Output:
+
+**********************************************/
